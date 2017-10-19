16baab8e20b8e2a7eef34416dd4224bb2a2d1fd7
David Linke <david.linke@neoris.com>
2017-10-18 20:28:41 -0400

    Create React App base

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+          <h1>David Linke</h1>
+          <h2>Web Designer and Developer.</h2>
+          <p>This is the new home of all the things I am proud of having built.</p>
+      </main>
+    );
+  }
+}
+
+export default App;
