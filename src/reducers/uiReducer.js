ab7e11fb21a6015b175dd8eb8a7c1b5a69f8be15
David Linke <david.linke@neoris.com>
2017-10-24 19:44:24 -0400

    Minor changes to css.

---
+++ src/reducers/uiReducer.js
@@ -0,0 +1,21 @@
+const defaultState = {
+  lightbox:{
+      visible:false,
+      url:'about:blank'
+  }
+};
+
+// create the reducers that will control the model
+const uiReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    // remember not to mutate the state
+    case 'UI_LIGHTBOX':
+        return Object.assign({}, state, {
+            lightbox:action.value
+        });
+    default:
+        return state;
+  }
+};
+
+export default uiReducer;
--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import { combineReducers, createStore } from 'redux';
 import defaultsReducer from  './reducers/defaultsReducer';
 import timelineReducer from  './reducers/timelineReducer';
+import uiReducer from  './reducers/uiReducer';
 
 // combine reducers - Although we have one only,
 // it is good to separate reducers through logical groups and then combine them.
 const appReducer = combineReducers({
     defaults:defaultsReducer,
-    timeline:timelineReducer
+    timeline:timelineReducer,
+    ui:uiReducer
 });
 
 const combinedReducers = (state,action) => {
